d597c29d2d54c8cfadd88958c40bd2f68ef9007d
Oleg Kalachev <okalachev@gmail.com>
2024-03-30 01:15:32 +0300

    Fix the example so it works with Serial Plotter

--- examples/MPU9250/MPU9250.ino
+++ examples/MPU9250/MPU9250.ino
@@ -18,7 +18,9 @@ void loop() {
   IMU.getGyro(gx, gy, gz);
   IMU.getAccel(ax, ay, az);
   IMU.getMag(mx, my, mz);
-  Serial.print("gx: "); Serial.print(gx); Serial.print(" "); Serial.print("gy: "); Serial.print(gy); Serial.print(" "); Serial.print("gz: "); Serial.print(gz);
-  Serial.print(" ax: "); Serial.print(ax); Serial.print(" "); Serial.print("ay: "); Serial.print(ay); Serial.print(" "); Serial.print("az: "); Serial.print(az);
-  Serial.print(" mx: "); Serial.print(mx); Serial.print(" "); Serial.print("my: "); Serial.print(my); Serial.print(" "); Serial.print("mz: "); Serial.println(mz);
+  // Print data for Serial Plotter:
+  Serial.print("gx:"); Serial.print(gx); Serial.print(" gy:"); Serial.print(gy); Serial.print(" gz:"); Serial.println(gz);
+  Serial.print(" ax:"); Serial.print(ax); Serial.print(" ay:"); Serial.print(ay); Serial.print(" az:"); Serial.print(az);
+  Serial.print(" mx:"); Serial.print(mx); Serial.print(" my:"); Serial.print(my); Serial.print(" mz:"); Serial.println(mz);
+  delay(50); // slow down the output
 }
