3c542cafc0281a33d12f50dbdd3b28f44744757f
Oleg Kalachev <okalachev@gmail.com>
2024-12-04 09:31:46 +0300

    Add example sketch for ICM20948

---
+++ examples/ICM20948/ICM20948.ino
@@ -0,0 +1,28 @@
+#include <ICM20948.h>
+#include <SPI.h>
+
+ICM20948 IMU(SPI); // no need to specify CS pin, the default pin is used automatically
+
+void setup() {
+  Serial.begin(115200);
+  bool success = IMU.begin();
+  if (!success) {
+    while (1) {
+      Serial.println("Failed to initialize IMU");
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  float gx, gy, gz, ax, ay, az, mx, my, mz;
+  IMU.waitForData(); // blockingly read the data, use IMU.read() for non-blocking read
+  IMU.getGyro(gx, gy, gz);
+  IMU.getAccel(ax, ay, az);
+  IMU.getMag(mx, my, mz);
+  // Print data for Serial Plotter:
+  Serial.print("gx:"); Serial.print(gx); Serial.print(" gy:"); Serial.print(gy); Serial.print(" gz:"); Serial.print(gz);
+  Serial.print(" ax:"); Serial.print(ax); Serial.print(" ay:"); Serial.print(ay); Serial.print(" az:"); Serial.print(az);
+  Serial.print(" mx:"); Serial.print(mx); Serial.print(" my:"); Serial.print(my); Serial.print(" mz:"); Serial.println(mz);
+  delay(50); // slow down the output
+}
